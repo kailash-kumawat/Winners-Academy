7afb9daf36a3e69e1bbd376f713b546542104028
Kailash Kumawat <kumawatkai369@gmail.com>
2025-09-13 11:47:34 +0530

    enhance student registration model with JWT token generation and improved password requirements

--- src/models/studentRegister.model.js
+++ src/models/studentRegister.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const studentRegisterSchema = new Schema(
   {
@@ -7,17 +8,20 @@ const studentRegisterSchema = new Schema(
       type: String,
       required: true,
       lowercase: true,
+      index: true,
+      trim: true,
     },
 
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
     },
 
     password: {
       type: String,
-      required: true,
+      required: [true, "Password required"],
     },
 
     studentImage: {
@@ -39,6 +43,22 @@ studentRegisterSchema.pre("save", async function (next) {
 studentRegisterSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
 // generate accessToken and refreshToken using jwt
+studentRegisterSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, name: this.name, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+studentRegisterSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
 
 export const Register = mongoose.model("Register", studentRegisterSchema);
